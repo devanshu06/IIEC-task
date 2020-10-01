4a07248dd1c1febd5942d5cf53e676ab77581fc8
shethneel <55236600+shethneel@users.noreply.github.com>
2020-10-01 07:33:25 +0530

    Update mainpage.py

--- mainpage.py
+++ mainpage.py
@@ -3,10 +3,11 @@
 print("content-type: text/html")
 print()
 
+#imporing libraries
 import cgi
 import subprocess
 
-
+#main logic
 cmd= cgi.FieldStorage()
 y= cmd.getvalue("x")
 print(subprocess.getoutput(y))
